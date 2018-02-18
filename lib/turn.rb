a5dddce31520bc6a2eb7403729837f07f1691169
Brian Ringley <brian.ringley@wework.com>
2018-02-18 22:49:10 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,34 +10,50 @@
 # end
 
 # shove all the helpers in this d00d 
-def turn 
+def turn(board) 
+  puts "Where do you want to make your move? Enter 1-9:"
+  until valid_move?(board_index)
+    puts "I'm sorry - you entered an invalid move!"
+    turn(board)
+  end
+  # input recorded in bin/turn with gets.strip
 end
 
 # print out the current state of the board 
 def display_board(board)
-  puts "  #{board[0]} | #{board[1]}  |  #{board[2]} "
+  puts "  #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts "  #{board[0]} | #{board[1]}  |  #{board[2]} "
+  puts "  #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts "  #{board[0]} | #{board[1]}  |  #{board[2]} "
+  puts "  #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# check for dumb dumb humans - return true if valid index/position not already taken
+# convert input to index
+def input_to_index(input)
+  index = input - 1 
+end
+
+# is the user input a valid move?
 def valid_move?(board, index)
-  
-  # if
-  # else 
-  #   position_taken?(board, index)
-  # end
-  
+  if !board[index].between?(0, 8) || !board[index].is_a?(Integer) || board[index] == nil
+    false
+  else 
+    !position_taken?(board, index)
+  end
 end
 
-# check if a position if already taken and use it as a help in #valid_move?
+# has this position been taken?
 def position_taken?(board, index)
+  if (board[index] == "X") || (board[index] == "O")
+    true 
+  else
+    false 
+  end
 end
   
-# make ur m00v
-def move(board, index, token)
+# record the move with a default token of X
+def move(board, index, token = "X")
+  board[index] = token
 end
 
 
