6df5ecf45c991cc76d4cd77dad50534c7462777e
Brian Ringley <brian.ringley@wework.com>
2018-02-18 22:19:06 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,45 @@
+# p s e u d o c o d e 
+# ask for input
+# get input
+# convert input to index
+# if index is valid
+#   make the move for index
+#   show the board
+# else
+#   ask for input again until you get a valid input
+# end
+
+# shove all the helpers in this d00d 
+def turn 
+end
+
+# print out the current state of the board 
+def display_board(board)
+  puts "  #{board[0]} | #{board[1]}  |  #{board[2]} "
+  puts "-----------"
+  puts "  #{board[0]} | #{board[1]}  |  #{board[2]} "
+  puts "-----------"
+  puts "  #{board[0]} | #{board[1]}  |  #{board[2]} "
+end
+
+# check for dumb dumb humans - return true if valid index/position not already taken
+def valid_move?(board, index)
+  
+  # if
+  # else 
+  #   position_taken?(board, index)
+  # end
+  
+end
+
+# check if a position if already taken and use it as a help in #valid_move?
+def position_taken?(board, index)
+end
+  
+# make ur m00v
+def move(board, index, token)
+end
+
+
+
+
